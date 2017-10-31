19a9d1e9d746d7802c67af4c855a016de8185ff2
Mohd Iqbal <iqbal.safian@gmail.com>
2017-11-01 05:56:59 +0800

    Knexfile

--- src/server/knexfile.js
+++ src/server/knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'bis',
-      user:     'iqbal',
-      password: 'iqbal007'
+      user:     'postgreuser',
+      password: 'password'
     },
     pool: {
       min: 2,
